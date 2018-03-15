5044db56d025d89bc0717dd7c8e1d91cb3baa78e
alvarogarcia1010 <00111317@uca.edu.sv>
2018-03-14 20:46:40 -0600

    Template

---
+++ src/template/java/TemplateJava.java
@@ -0,0 +1,14 @@
+
+package template.java;
+
+/*
+ * @author Alvaro Garcia 
+ */
+
+public class TemplateJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
