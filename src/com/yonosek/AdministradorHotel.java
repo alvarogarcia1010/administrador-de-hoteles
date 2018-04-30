0b336c9e5793c7a45c59414ab757d4759286e705
alvarogarcia1010 <00111317@uca.edu.sv>
2018-04-29 22:56:02 -0600

    Issue #1

---
+++ src/com/yonosek/AdministradorHotel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yonosek;
+
+/**
+ *
+ * @author Alvaro García <alvarogarcia1010 at github.com>
+ */
+public class AdministradorHotel {
+    
+}
