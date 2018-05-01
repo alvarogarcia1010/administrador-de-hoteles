edd280af22af22ef91e1fe928f6e71dad0d9aaa6
alvarogarcia1010 <00111317@uca.edu.sv>
2018-05-01 17:28:34 -0600

    Issue #5

--- src/com/yonosek/AdministradorHotel.java
+++ src/com/yonosek/AdministradorHotel.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.yonosek;
+
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
  * @author Alvaro García <alvarogarcia1010 at github.com>
  */
 public class AdministradorHotel {
-    
+
+    //Formato Moneda   
+    static DecimalFormat formatoMoneda = new DecimalFormat("$ #.00");
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Administrador Hotel!!!");
+        
+        Menu menu = Menu.getInstance();
+       
+        menu.mostrar();
+        
+    }
 }
--- src/com/yonosek/Hotel.java
+++ src/com/yonosek/Hotel.java
@@ -1,15 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.yonosek;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
  * @author Alvaro García <alvarogarcia1010 at github.com>
  */
 public class Hotel {
-    /*HOLAAAA*/
+    private int idHotel;
+    private String nombreHotel;
+    private float precioBase;
+    private ArrayList<Paquete> clientesHotel;
+    private HashMap<String, Paquete> pisosHotel;
+    private HashMap<Integer, Paquete> habitacionesHotel;
+    private HashMap<Integer, Paquete> paquetesHotel;
     
+    /**Constructor Vacio*/
+    public Hotel(){
+        this.clientesHotel = new ArrayList<>();
+        this.pisosHotel = new HashMap<>();
+        this.habitacionesHotel = new HashMap<>();
+        this.paquetesHotel = new HashMap<>();
+    }
+    
+    /**
+     * Constructor General
+     * @param idHotel
+     * @param nombreHotel
+     * @param precioBase 
+     */
+    public Hotel(int idHotel, String nombreHotel, float precioBase) {
+        this.idHotel = idHotel;
+        this.nombreHotel = nombreHotel;
+        this.precioBase = precioBase;
+        this.clientesHotel = new ArrayList<>();
+        this.pisosHotel = new HashMap<>();
+        this.habitacionesHotel = new HashMap<>();
+        this.paquetesHotel = new HashMap<>();
+    }
+
+    public int getIdHotel() {
+        return idHotel;
+    }
+
+    public void setIdHotel(int idHotel) {
+        this.idHotel = idHotel;
+    }
+
+    public String getNombreHotel() {
+        return nombreHotel;
+    }
+
+    public void setNombreHotel(String nombreHotel) {
+        this.nombreHotel = nombreHotel;
+    }
+
+    public float getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(float precioBase) {
+        this.precioBase = precioBase;
+    }
+
 }
