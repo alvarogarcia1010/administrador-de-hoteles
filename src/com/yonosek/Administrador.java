d99cc98ad2a2fc984faebb4e198dd07e4843daf1
ryanes97 <00082717@uca.edu.sv>
2018-05-02 09:38:26 -0600

    Issue #2

---
+++ src/com/yonosek/Administrador.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yonosek;
+
+/**
+ *
+ * @author ryane
+ */
+public class Administrador {
+    private String username;
+    private String password;
+
+    public Administrador(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/com/yonosek/Persona.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yonosek;
+
+/**
+ *
+ * @author ryane
+ */
+public class Persona {
+    private String DUI;
+    private String nombre;
+    private String apellido;
+    
+     public Persona(String DUI, String nombre, String apellido) {
+        this.DUI = DUI;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public void setDUI(String DUI) {
+        this.DUI = DUI;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDUI() {
+        return DUI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+}
