6c997f942119127b6fb907acd4af9ffff531b5e1
Dalcoleas <00048015@uca.edu.sv>
2018-05-03 17:39:54 -0600

    Issue #4

--- src/com/yonosek/AdministradorHotel.java
+++ src/com/yonosek/AdministradorHotel.java
@@ -17,21 +17,156 @@ public class AdministradorHotel {
 
 
     public static void main(String[] args) {
-        System.out.println("Administrador Hotel!!!");
+        
+        System.out.println("Administrador Hotel");
+        
+        Hotel VillaRafinha = new Hotel(1,"VillaRafinha",100.00,150.00);
         
         Menu menu = Menu.getInstance();
-       
-        menu.mostrar();
-                
+        
+        ejecutarMenu(menu);
+        
+        }
+    
+        public static void ejecutarMenu(Menu menu){   
+        int opcion = 5;
+        int opcionSec = 5;
+        Scanner leer = new Scanner(System.in);
+
+        while (opcion != 4) {
+            menu.opcionesMenu(); //menu.opcionesMenu
+            try {
+                System.out.print("Elija opcion deseada: ");
+                opcion = leer.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        System.out.print("\n");
+                        menu.opcionesMenuClientes(); //menu.menuClientes;
+                        System.out.print("Elija opcion deseada: ");
+                        opcionSec = leer.nextInt();
+                        System.out.print("\n");
+                        switch (opcionSec) {
+                            case 1:
+                                /* Registrar Cliente */
+                                System.out.println("/* Registrar Cliente */");
+                                crearCliente();
+                                break;
+                            case 2:
+                                /* Modificar Cliente */
+                                System.out.println("/* Modificar Cliente */");
+                                break;
+                            case 3:
+                                /* Eliminar Cliente */
+                                System.out.println("/* Eliminar Cliente */");
+                                break;
+                            case 4:
+                                /* Mostrar Clientes */
+                                System.out.println("/* Mostrar Clientes */");
+                                break;
+                            case 5:
+                                break;
+                            default:
+                                System.out.println("Por favor ingrese una opcion valida");
+                                System.out.print("\n");
+                        }
+                        break;
+                    case 2:
+                        System.out.print("\n");
+                        menu.opcionesMenuReservaciones();
+                        System.out.print("Elija opcion deseada: ");
+                        opcionSec = leer.nextInt();
+                        System.out.print("\n");
+                        switch(opcionSec){
+                            case 1:
+                                /* Agregar Reservacion */
+                                System.out.println("/* Agregar Reservacion */");
+                                break;
+                            case 2:
+                                /* Modificar Reservacion */
+                                System.out.println("/* Modificar Reservacion */");
+                                break;
+                            case 3:
+                                /* Eliminar Reservacion */
+                                System.out.println("/* Eliminar Reservacion */");
+                                break;
+                            case 4:
+                                /* Mostrar reservaciones por semana */
+                                System.out.println("/* Mostrar reservaciones por semana */");
+                                break;
+                            case 5:
+                                break;
+                            default:
+                                System.out.println("Por favor ingrese una opcion valida");
+                                System.out.print("\n");
+                        }
+                        break;
+                    case 3:
+                        System.out.print("\n");
+                        menu.opcionesMenuServicios();
+                        System.out.print("Elija opcion deseada: ");
+                        opcionSec = leer.nextInt();
+                        System.out.print("\n");
+                        switch(opcionSec){
+                            case 1:
+                                /* Habilitar/Deshabilitar habitacion */
+                                System.out.println("/* Habilitar/Deshabilitar habitacion */");
+                                break;
+                            case 2:
+                                /* Habilitar/Deshabilitar piso */
+                                System.out.println("/* Habilitar/Deshabilitar piso */");
+                                break;
+                            case 3:
+                                /* Modificar precio base de habitacion sencilla*/
+                                System.out.println("/* Modificar precio base de habitacion sencilla */");
+                                break;
+                            case 4:
+                                /* Modificar precio de habitacion doble */
+                                System.out.println("/* Modificar precio base de habitacion doble */");
+                                break;
+                            case 5:
+                                /* Agregar paquete */
+                                System.out.println("/* Agregar paquete */");
+                                break;
+                            case 6:
+                                /* Modificar paquete */
+                                System.out.println("/* Modificar paquete */");
+                                break;
+                            case 7:
+                                /* Eliminar paquete */
+                                System.out.println("/* Eliminar paquete */");
+                                break;
+                            case 8:
+                                /* Mostrar paquetes */
+                                System.out.println("/* Mostrar paquetes */");
+                                break;
+                            case 9:
+                                break;                                    
+                            default:
+                                System.out.println("Por favor ingrese una opcion valida");
+                                System.out.print("\n");
+                        }
+                        break;
+                    case 4:
+                        System.out.print("\n");
+                        System.out.println("Saliendo...");
+                        break;
+                    default:
+                        System.out.println("Por favor ingrese una opcion valida");
+                        System.out.print("\n");
+                }
+            } catch (InputMismatchException e) {
+                System.err.println("Por favor, ingrese un numero");
+                leer.nextLine();
+            }
+        }
         
     }
     
-    public Hotel crearHotel(){
+    //public Hotel crearHotel(){
 
-    Hotel hotel = new Hotel(1,"hotel",100.00,150.00);
-
-    return hotel;
-    }
+    //return hotel;
+    //}
     
     
     public Habitacion crearHabitacion(){
@@ -80,7 +215,7 @@ public class AdministradorHotel {
     return reservacion;
     }
     
-    public Cliente crearCliente(){
+    public static Cliente crearCliente(){
         Cliente cliente = new Cliente();
         Scanner leer = new Scanner(System.in);
         try{
